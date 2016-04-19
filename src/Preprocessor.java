8c245a4a2c0cfb6ee51b717d88570f520fca1c31
mortenmg <mortenmg@gmail.com>
2016-04-19 18:31:47 +0200

    Corridors is now being found by preprocessor and a map with only walls and detected corridors are printed in the console for debugging.

--- src/Preprocessor.java
+++ src/Preprocessor.java
@@ -18,9 +18,17 @@ public class Preprocessor {
     //Testing arrays for making goal lists unprioritized at first
     private ArrayList<Point> goalPoints = new ArrayList<Point>();
     private ArrayList<Point> boxPoints = new ArrayList<Point>();
+    private HashMap<Integer,Boolean> corridorTypes = new HashMap<Integer,Boolean>();
+
+    private Cell[][] map = new Cell[70][70];
 
     public Preprocessor(BufferedReader serverMessages) {
         this.serverMessages = serverMessages;
+        initializeCorridorTypes();
+    }
+
+    private void initializeCorridorTypes() {
+
     }
 
     public List<Agent> readMap() throws IOException {
@@ -41,12 +49,15 @@ public class Preprocessor {
 
         // Read lines specifying level layout
         while ( !line.equals( "" ) ) {
+
             for (int i = 0; i < line.length(); i++) {
                 char id = line.charAt(i);
                 if ('0' <= id && id <= '9') //If agent
                     agents.add(new Agent(id, colors.get(id)));
-                else if (id == '+') //If wall
-                    walls[levelLine][i] = id;
+                else if (id == '+') { //If wall
+                    this.walls[levelLine][i] = id;
+//                    System.err.println("Found wall at ("+ levelLine + "," + i +"): " + walls[levelLine][i]);
+                }
                 else if ('a' <= id && id <= 'z' ){ //If goal
                     goals.put(new Point(levelLine,i),id);
                     goalPoints.add(new Point(levelLine,i));
@@ -56,8 +67,11 @@ public class Preprocessor {
                     boxPoints.add(new Point(levelLine,i));
                 }
             }
+            levelLine ++;
             line = serverMessages.readLine();
         }
+        findCorridors();
+        printCorridorMap();
         return agents;
     }
 
@@ -84,7 +98,117 @@ public class Preprocessor {
 
     //Need a datastructure for corridors
     private void findCorridors(){
-        //return corridors
+        char c;
+        int id = 48; //Ascii character '0'
+
+        for (int i = 1; i < walls.length-1;i++) {
+            for (int j = 1; j < walls[0].length-1; j++){
+
+                //If cell is a wall, skip
+                c = walls[i][j];
+                if (c == '+') continue;
+
+                //If cell is empty, analyse
+                else if(isCorridor(i,j)){
+
+//                    walls[i][j] = 'c';
+
+                    if(assignId(i,j,id)){
+                        id++;
+                    }
+                }
+//                isCorridorSum(i,j);
+            }
+        }
+    }
+
+    private boolean isCorridorSum(int x, int y){
+        // Iterate 3x3 kernal
+        int value = 1;
+        int score = 0;
+        String s = "";
+        System.err.println("Kernal " + x + "," + y);
+        for (int i = x-1; i <= x+1; i++){
+            for (int j = y-1; j <= y+1; j++){
+                if(walls[i][j] == '+')
+                    score += value;
+                value++;
+                s = s + walls[i][j];
+            }
+            System.err.println(s);
+            s = "";
+        }
+        walls[x][y] = (char)score;
+        return false;
+    }
+    private boolean isCorridor(int x, int y){
+
+        //Vertical corridor
+        if((walls[x-1][y] !='+' || walls[x+1][y] != '+') && walls[x][y-1] == '+' && walls[x][y+1] == '+'){
+            return true;
+        }
+        //Horizontal corridor
+        else if(walls[x-1][y] == '+' && walls[x+1][y] == '+' && (walls[x][y-1] != '+' || walls[x][y+1] != '+')){
+            return true;
+        }
+        //Corners - assume that you can escape from a free cell that is not a dead end
+        //Corner LD
+        else if(walls[x][y-1] == '+' && walls[x+1][y] == '+' && walls[x-1][y+1] == '+')
+            return true;
+        //Corner RD
+        else if(walls[x][y-1] == '+' && walls[x-1][y] == '+' && walls[x+1][y+1] == '+')
+            return true;
+        //Corner LU
+        else if(walls[x][y+1] == '+' && walls[x+1][y] == '+' && walls[x-1][y-1] == '+')
+            return true;
+        //Corner RU
+        else if(walls[x][y+1] == '+' && walls[x-1][y] == '+' && walls[x+1][y-1] == '+')
+            return true;
+
+        return false;
+    }
+
+    private boolean assignId(int x, int y, int id){
+        char c = (char)0;
+        if (walls[x-1][y] > '+'){
+            c = walls[x-1][y];
+        }
+        else if (walls[x+1][y] > '+'){
+            c = walls[x+1][y];
+        }
+        else if (walls[x][y-1] > '+'){
+            c = walls[x][y-1];
+        }
+        else if (walls[x][y+1] > '+'){
+            c = walls[x][y+1];
+        }
+        if (c > (char)0){
+            walls[x][y] = c;
+            return false;
+        }
+        else{
+            walls[x][y] = (char)id;
+            return true;
+        }
+    }
+
+    public void printCorridorMap(){
+        System.err.println("Corridor map print out");
+        String s = "";
+
+        for (int i = 0; i < walls.length;i++) {
+            s = "";
+            if(walls[i][0] < ' ') //no more lines in map
+                break;
+
+            for (int j = 0; j < walls[0].length; j++){
+                if(walls[i][j] > ' ')
+                    s = s + walls[i][j];
+                else
+                    s = s + " ";
+            }
+            System.err.println(s);
+        }
     }
 
 }
